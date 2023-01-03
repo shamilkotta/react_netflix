67367238bdd39698b80e50f2eb1655c071fdd22d
shamilkotta <shamilkotta99@gmail.com>
2023-01-03 20:36:08 +0530

    feat: hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar/Navbar";
 
 function Home() {
   return (
     <div style={{ backgroundColor: "black" }}>
       <Navbar />
+      <Hero />
     </div>
   );
 }
