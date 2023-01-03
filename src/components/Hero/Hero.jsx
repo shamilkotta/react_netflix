c6cd76f3a00bf5d556259704badc03186abb00b2
shamilkotta <shamilkotta99@gmail.com>
2023-01-03 20:38:34 +0530

    test: husky

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import heroImage from "./wednesday.jpg"
-import playImage from "./play.png"
-import netflixImage from "./netflix.png"
-import "./Hero.css"
+import React from "react";
+import heroImage from "./wednesday.jpg";
+import playImage from "./play.png";
+import netflixImage from "./netflix.png";
+import "./Hero.css";
 
 function Hero() {
-
-
   return (
-    <div className='hero' style={{ backgroundImage:`url(${heroImage})` }}>
-      <div className='hero-content' style={{height: "100%"}}>
-        <p> <img height="22px" width="25px" src={netflixImage} alt="netflix" /> SERIES</p>
+    <div className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
+      <div className="hero-content" style={{ height: "100%" }}>
+        <p>
+          {" "}
+          <img
+            height="22px"
+            width="25px"
+            src={netflixImage}
+            alt="netflix"
+          />{" "}
+          SERIES
+        </p>
         <h2>WEDNESDAY</h2>
         <div>
-          <button type='button'><img src={playImage} width="12px" alt="play" />Play</button>
-          <button type='button'> <span>i</span> More Info</button>
+          <button type="button">
+            <img src={playImage} width="12px" alt="play" />
+            Play
+          </button>
+          <button type="button">
+            {" "}
+            <span>i</span> More Info
+          </button>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
