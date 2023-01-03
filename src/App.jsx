dd727bac2858e048a1c7093516469a726168a835
shamilkotta <shamilkotta99@gmail.com>
2023-01-03 16:52:55 +0530

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="header">
-        <video src="" />
-      </div>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+function Home() {
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
