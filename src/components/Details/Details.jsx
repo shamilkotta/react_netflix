36dce143f661d9cd2c0e6e1456fddc6679a2d82c
shamilkotta <shamilkotta99@gmail.com>
2023-01-04 10:34:16 +0530

    feat: details card

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Details.css";
+
+function Details() {
+  return (
+    <div className="details">
+      <div className="details-card">
+        <div className="details-poster">
+          <img src="" alt="poster" />
+        </div>
+        <div className="details-data">
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+            aspernatur harum cupiditate.
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Cupiditate facilis cumque reprehenderit quam,
+            deserunt ipsum facere omnis reiciendis obcaecati temporibus fugiat
+            sequi iste esse quod accusantium illo adipisci impedit suscipit vel,
+            repellendus molestiae culpa fuga. Dolorem delectus quisquam ipsam
+            quo veniam in distinctio cumque provident. Quidem voluptate sapiente
+            at sequi voluptatum. Quasi illo dicta deserunt quia? Reprehenderit
+            nihil optio sit ipsum necessitatibus quod maiores neque! Placeat
+            tempora laudantium voluptates, temporibus doloremque, consequuntur
+            in assumenda quia veritatis quae ab. Consequatur molestiae ab eum ea
+            enim dolor, rerum, quod odit consectetur cupiditate, in aperiam id
+            accusantium corrupti incidunt nemo repellendus ratione autem.
+            consectetur adipisicing elit. Veniam suscipit pariatur at modi magni
+            enim placeat illum reiciendis iusto assumenda blanditiis eligendi
+            cum explicabo dolores omnis culpa quas a tempore, reprehenderit,
+            harum totam quod? Recusandae modi aut iusto dolor, nemo aliquid
+            sint! Sunt unde enim asperiores accusantium vitae ut nihil?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Details;
