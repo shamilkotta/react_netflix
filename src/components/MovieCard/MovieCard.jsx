29081b9d7c56a142e982810d7953458ae45b7315
shamilkotta <shamilkotta99@gmail.com>
2023-01-04 15:09:17 +0530

    feat: dynamic data

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,20 +3,22 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 
 import React from "react";
-import brbd from "./brba.jpeg";
+import constants from "../../utils/constants";
 import playImg from "./play.png";
 import "./MovieCard.css";
 
-function MovieCard({ showModal }) {
+function MovieCard({ img, title, ele, showModal, setModalData }) {
   return (
     <div
       className="movie-card"
-      style={{ backgroundImage: `url(${brbd})` }}
+      style={{ backgroundImage: `url(${constants.imgUrl}${img})` }}
       onClick={() => {
+        setModalData(ele);
         showModal(true);
       }}
     >
       <img src={playImg} alt="play" width="50px" />
+      <h3>{title}</h3>
     </div>
   );
 }
