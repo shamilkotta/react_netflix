2773e6af4c6f21e41fb2923db16e6b8d516862a7
shamilkotta <shamilkotta99@gmail.com>
2023-01-04 09:39:56 +0530

    feat: horizontal scrolling

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useEffect, useRef, useState } from "react";
 import MovieCard from "../MovieCard/MovieCard";
 import "./Collection.css";
 
-function Collection() {
+function Collection({ collection }) {
+  const ref = useRef(null);
+  const [width, setWidth] = useState();
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    ref.current.addEventListener("scroll", () => {
+      setScrollPos(ref.current.scrollLeft);
+    });
+    return () => {
+      ref.current.removeEventListener("scroll", () => {
+        setScrollPos(ref.current.scrollLeft);
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    setWidth(ref.current.offsetWidth);
+  }, []);
+
+  const scrollElement = (amount) => {
+    ref.current.scrollLeft += amount;
+  };
+
   return (
     <div className="collection-container">
-      <h3 className="collection-head">New Releases</h3>
-      <div className="collection">
+      <h3 className="collection-head">{collection}</h3>
+      {scrollPos > 0 && (
+        <button
+          className="collection-left-arrow"
+          type="button"
+          onClick={() => {
+            scrollElement(-100);
+          }}
+        >
+          {" "}
+          <span>&#8249;</span>{" "}
+        </button>
+      )}
+      <div className="collection" ref={ref}>
         <MovieCard />
         <MovieCard />
         <MovieCard />
@@ -24,6 +60,18 @@ function Collection() {
         <MovieCard />
         <MovieCard />
       </div>
+      {scrollPos + width < 4500 && (
+        <button
+          className="collection-right-arrow"
+          type="button"
+          onClick={() => {
+            scrollElement(100);
+          }}
+        >
+          {" "}
+          <span>&rsaquo;</span>{" "}
+        </button>
+      )}
     </div>
   );
 }
