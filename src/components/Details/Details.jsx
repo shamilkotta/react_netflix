8498be47cb604f6370bd3acc57b687da4032c6e3
shamilkotta <shamilkotta99@gmail.com>
2023-01-04 15:08:10 +0530

    feat: dynamic data

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,14 +2,17 @@
 
 import React from "react";
 import "./Details.css";
+import constants from "../../utils/constants";
 
-function Details({ showModal }) {
+function Details({ showModal, data }) {
   return (
     <div className="details">
       <div className="details-card">
-        <div className="details-poster">
-          <img src="" alt="poster" />
-        </div>
+        <img
+          src={`${constants.imgUrl}${data.poster}`}
+          alt="poster"
+          className="details-poster"
+        />
         <div className="details-data">
           <button
             type="button"
@@ -18,29 +21,10 @@ function Details({ showModal }) {
           >
             X
           </button>
-          <h3>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
-            aspernatur harum cupiditate.
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Cupiditate facilis cumque reprehenderit quam,
-            deserunt ipsum facere omnis reiciendis obcaecati temporibus fugiat
-            sequi iste esse quod accusantium illo adipisci impedit suscipit vel,
-            repellendus molestiae culpa fuga. Dolorem delectus quisquam ipsam
-            quo veniam in distinctio cumque provident. Quidem voluptate sapiente
-            at sequi voluptatum. Quasi illo dicta deserunt quia? Reprehenderit
-            nihil optio sit ipsum necessitatibus quod maiores neque! Placeat
-            tempora laudantium voluptates, temporibus doloremque, consequuntur
-            in assumenda quia veritatis quae ab. Consequatur molestiae ab eum ea
-            enim dolor, rerum, quod odit consectetur cupiditate, in aperiam id
-            accusantium corrupti incidunt nemo repellendus ratione autem.
-            consectetur adipisicing elit. Veniam suscipit pariatur at modi magni
-            enim placeat illum reiciendis iusto assumenda blanditiis eligendi
-            cum explicabo dolores omnis culpa quas a tempore, reprehenderit,
-            harum totam quod? Recusandae modi aut iusto dolor, nemo aliquid
-            sint! Sunt unde enim asperiores accusantium vitae ut nihil?
-          </p>
+          <div>
+            <h3>{data.title}</h3>
+            <p>{data.overview}</p>
+          </div>
         </div>
       </div>
     </div>
