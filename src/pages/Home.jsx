1887558696f1164c6b75fce64074fd4bc655cf27
shamilkotta <shamilkotta99@gmail.com>
2023-01-04 15:12:35 +0530

    feat: collection looping

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
+/* eslint-disable react/no-array-index-key */
+
 import React, { useState } from "react";
 import Collection from "../components/Collection/Collection";
 import Details from "../components/Details/Details";
 import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar/Navbar";
 import "./Home.css";
+import collection from "../utils/collection";
 
 function Home() {
   const [showModal, setShowModal] = useState(false);
+  const [modalData, setModalData] = useState({});
   return (
     <div style={{ backgroundColor: "black" }}>
       {showModal && (
@@ -14,23 +18,25 @@ function Home() {
           showModal={(res) => {
             setShowModal(res);
           }}
+          data={modalData}
         />
       )}
       <Navbar />
       <Hero />
       <div className="movies-collection">
-        <Collection
-          collection="New Realess"
-          showModal={(res) => {
-            setShowModal(res);
-          }}
-        />
-        <Collection
-          collection="Trending"
-          showModal={(res) => {
-            setShowModal(res);
-          }}
-        />
+        {collection.map((ele, index) => (
+          <Collection
+            key={index}
+            collection={ele.collection}
+            url={ele.url}
+            showModal={(res) => {
+              setShowModal(res);
+            }}
+            setModalData={(res) => {
+              setModalData(res);
+            }}
+          />
+        ))}
       </div>
     </div>
   );
