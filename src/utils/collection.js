51c2613923de3c01e174cbee1fb9dc303908c26d
shamilkotta <shamilkotta99@gmail.com>
2023-01-04 15:05:46 +0530

    setup: constants and collections

---
+++ src/utils/collection.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    collection: "Trending",
+    url: "/trending/all/week?api_key=a55d3ab1ab1034bd95dd728b80bf3432&language=en-US",
+  },
+  {
+    collection: "Action",
+    url: "/discover/movie?api_key=a55d3ab1ab1034bd95dd728b80bf3432&with_genres=28",
+  },
+  {
+    collection: "Originals",
+    url: "/discover/tv?api_key=a55d3ab1ab1034bd95dd728b80bf3432&with_networks=213",
+  },
+  // {
+  //   collection: "Comedy Movies",
+  //   url: "/discover/movie?api_key=$a55d3ab1ab1034bd95dd728b80bf3432&with_genres=35"
+  // },
+  // {
+  //   collection: "Horror Movies",
+  //   url: "/discover/movie?api_key=$a55d3ab1ab1034bd95dd728b80bf3432&with_genres=27"
+  // },
+  // {
+  //   collection: "Action Movies",
+  //   url: "/discover/movie?api_key=$a55d3ab1ab1034bd95dd728b80bf3432&with_genres=28"
+  // },
+  // {
+  //   collection: "Romance Movies",
+  //   url: "/discover/movie?api_key=$a55d3ab1ab1034bd95dd728b80bf3432&with_genres=10749"
+  // },
+  // {
+  //   collection: "Documentaries",
+  //   url: "/discover/movie?api_key=$a55d3ab1ab1034bd95dd728b80bf3432&with_genres=99"
+  // },
+];
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export default {
+  baseUrl: "https://api.themoviedb.org/3",
+  imgUrl: "https://image.tmdb.org/t/p/w500",
+  apiKey: "",
+};
